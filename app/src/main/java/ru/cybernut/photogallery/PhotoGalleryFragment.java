24289b5148fb4df1f43990e983f146b0adb9da47
CyberNut <cy6ernut@gmail.com>
2019-02-12 22:50:55 +0300

    1 part

--- app/src/main/java/ru/cybernut/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/ru/cybernut/photogallery/PhotoGalleryFragment.java
@@ -1,10 +1,105 @@
 package ru.cybernut.photogallery;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = "PhotoGalleryFragment";
+
+    private RecyclerView recyclerView;
+    private List<GalleryItem> galleryItems = new ArrayList<>();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        new FetchItemTask().execute();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, parent, false);
+        recyclerView = (RecyclerView) v.findViewById(R.id.photo_recycler_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        setupAdapter();
+        return v;
+    }
+
+    private void setupAdapter() {
+        if(isAdded()) {
+            recyclerView.setAdapter(new PhotoAdapter(galleryItems));
+        }
+    }
+
+    private class FetchItemTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+        @Override
+        protected List<GalleryItem> doInBackground(Void... voids) {
+            return new FlickrFetchr().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> items) {
+            galleryItems = items;
+            setupAdapter();
+        }
+    }
 
     public static Fragment newInstance() {
         return new PhotoGalleryFragment();
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+
+        public PhotoHolder(@NonNull View itemView) {
+            super(itemView);
+            this.textView = (TextView)itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem item) {
+            textView.setText(item.toString());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+
+        private List<GalleryItem> galleryItemList;
+
+        public PhotoAdapter(List<GalleryItem> galleryItemList) {
+            this.galleryItemList = galleryItemList;
+        }
+
+        @NonNull
+        @Override
+        public PhotoHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int position) {
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PhotoHolder photoHolder, int position) {
+            GalleryItem galleryItem = galleryItemList.get(position);
+            photoHolder.bindGalleryItem(galleryItem);
+        }
+
+        @Override
+        public int getItemCount() {
+            return galleryItemList.size();
+        }
     }
 }

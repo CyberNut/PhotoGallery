b1e9c4f90ac927e12e48e3c9f88e209e25031142
Andrey <cy6ernut@gmail.com>
2019-02-12 16:58:10 +0300

    Initial commit

---
+++ app/src/main/java/ru/cybernut/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,10 @@
+package ru.cybernut.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryFragment extends Fragment {
+
+    public static Fragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+}
